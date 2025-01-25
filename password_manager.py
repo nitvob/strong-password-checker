9a6602dc478399abb5f3ac51609863116f10d598
Anish Vobilisetti <avob@Sris-MacBook-Air.local>
2025-01-25 15:41:24 -0800

    im done

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def is_strong_password(password: str) -> bool:
     """
     Check if the provided password meets the following criteria:
@@ -15,7 +18,31 @@ def is_strong_password(password: str) -> bool:
         bool: True if password meets all criteria, False otherwise
     """
     # TODO: Implement the password checking logic here
-    pass
+    def isspecial(char):
+        '''
+        special means NOT lowercase letter AND NOT uppercase letter AND NOT digit 
+        not char.islower() && not char.isupper() && not char.isdigit()
+        '''
+        if not char.islower() and not char.isupper() and not char.isdigit():
+            return True
+        else: 
+            return False 
+
+    if len(password) < 8:
+        return False
+    elif len(password) > 100: 
+        return False
+    elif not any(char.islower() for char in password): 
+        return False
+    elif not any(char.isupper() for char in password):
+        return False 
+    elif not any(char.isdigit() for char in password): 
+        return False
+    elif not any(isspecial(char) for char in password):
+        return False
+    else:
+        return True
+        
 
 
 def generate_strong_password(length: int) -> str:
@@ -33,4 +60,39 @@ def generate_strong_password(length: int) -> str:
         str: A strong password meeting all criteria
     """
     # TODO: Implement the password generation logic here
-    pass
+    '''
+    what if the length they give us is <8 or >100, riase an error
+    get a random lowercase letter. stick this into the string
+        how do we get a random lowercase letter (google it)
+    get a random uppercase letter. stick this into the string
+        how do we get a random uppercase letter (google it)
+    get a random digit. stick this into the string
+        how do we get a random digit (random.randint(0, 9))
+    get a random special. stick this into the string
+    
+    what about length - 4 remaing chars?
+        put random chars for the rest
+    '''
+    if length < 8 or length > 100: 
+        raise ValueError("Invalid length")
+    
+    randomLower = random.choice(string.ascii_lowercase)
+    randomUpper = random.choice(string.ascii_uppercase)
+    randomDig = str(random.randint(0, 9))
+    randomSpecial = random.choice(string.punctuation)
+    
+    password = randomLower + randomUpper + randomDig + randomSpecial 
+
+    for i in range(length - 4):
+        randomLower = random.choice(string.ascii_lowercase)
+        randomUpper = random.choice(string.ascii_uppercase)
+        randomDig = str(random.randint(0, 9))
+        randomSpecial = random.choice(string.punctuation)
+        randomChar = random.choice([randomLower, randomUpper, randomDig, randomSpecial])
+        password += randomChar
+
+    return password
+    
+
+        
+   
